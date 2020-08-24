5f2b3a324f435daab0be0d0591c6985f35cd500d
anabeatriiz <anabeatrizvianaa@gmail.com>
2020-08-24 19:21:21 -0300

    initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Routes from './pages/Landing/routers';
+import '../src/styles/global.css';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
